ced7b3675df79f47ceedf10374d964c6c12a5778
Michael Kleehammer <michael@kleehammer.com>
2015-03-09 11:09:15 -0500

    Added index.js and readme

---
+++ README.md
@@ -0,0 +1,13 @@
+
+A manual port of the WHATWG Streams reference implementation into something that
+can be used on node 0.12 and io.js.
+
+```
+var ws5 = require('ws5-streams');
+
+// ws5.ReadableStream
+// ws5.WritableStream
+// ws5.TransformStream
+// ws5.ByteLengthQueuingStrategy
+// ws5.CountQueuingStrategy
+```
---
+++ index.js
@@ -0,0 +1,6 @@
+
+exports.ReadableStream = require('./lib/readable-stream').ReadableStream;
+exports.WritableStream = require('./lib/writable-stream').WritableStream;
+exports.TransformStream = require('./lib/transform-stream').TransformStream;
+exports.ByteLengthQueuingStrategy = require('./lib/byte-length-queuing-strategy.js');
+exports.CountQueuingStrategy = require('./lib/count-queuing-strategy.js');
